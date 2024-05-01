4813eb21a7ce16243d894115c559a9c00e40fd7a
Portian02 <135663498+Portian02@users.noreply.github.com>
2024-05-01 15:57:36 -0600

    diseño de input

--- script.js
+++ script.js
@@ -11,6 +11,31 @@ boton.addEventListener("click", function () {
     
 })
 
+let nube = document.getElementById('nube');
+let fondo = document.getElementById('fondo');
 
- 
-    
+
+nube.addEventListener("click", () => {
+   fondo.style.backgroundColor = "#c3e5e6"
+   
+
+})
+
+let dark = document.getElementById('oscuro');
+let fondo1 = document.getElementById('fondo');
+
+
+oscuro.addEventListener("click", () => {
+   fondo.style.backgroundColor = "#808a8a"
+   
+
+})
+let rain = document.getElementById('lluvia');
+let fondo2 = document.getElementById('fondo');
+
+
+lluvia.addEventListener("click", () => {
+   fondo.style.backgroundColor = "#83bdd0"
+   
+
+})
